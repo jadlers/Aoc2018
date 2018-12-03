6b1d7ae826eaf28a62192a7394f07bfe3cffaff9
Jacob Adlers <jacob.adlers@gmail.com>
2018-12-03 12:33:40 +0100

    Finish Day 3 part 1

---
+++ day3/day3.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	util "github.com/jadlers/AoC2018/util"
+)
+
+type claim struct {
+	id      int
+	offsetX int
+	offsetY int
+	width   int
+	height  int
+}
+
+func main() {
+	lines := util.ReadLines()
+
+	fmt.Printf("Part 1: %v\n", Part1(lines))
+	// fmt.Printf("Part 2: %v\n", Part2(lines))
+}
+
+func Part1(lines []string) int {
+	// Turn input into the form of claims
+	claims := createClaimsSlice(lines)
+	overlapping := [1000][1000]int{}
+
+	for _, claim := range claims {
+		for x := 0; x < claim.width; x++ {
+			for y := 0; y < claim.height; y++ {
+				overlapping[x+claim.offsetX][y+claim.offsetY]++
+				// fmt.Printf("overlapping[%v][%v] = %v\n", x+claim.offsetX, y+claim.offsetY, overlapping[x+claim.offsetX][y+claim.offsetY])
+			}
+		}
+	}
+
+	sum := 0
+	for x := 0; x < len(overlapping); x++ {
+		for y := 0; y < len(overlapping[0]); y++ {
+			if overlapping[x][y] > 1 {
+				sum += 1
+			}
+		}
+	}
+
+	return sum
+}
+
+func createClaimsSlice(lines []string) []claim {
+	var claims []claim
+	for _, line := range lines {
+		var id, offsetX, offsetY, width, height int
+		_, err := fmt.Sscanf(line, "#%d @ %d,%d: %dx%d\n", &id, &offsetX, &offsetY, &width, &height)
+		if err != nil {
+			fmt.Println(err)
+		}
+		claims = append(claims, claim{id, offsetX, offsetY, width, height})
+	}
+	return claims
+}
---
+++ day3/day3_test.go
@@ -0,0 +1,23 @@
+package main
+
+import "testing"
+
+func TestPart1(t *testing.T) {
+	type args struct {
+		lines []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"Example given", args{[]string{"#1 @ 1,3: 4x4", "#2 @ 3,1: 4x4", "#3 @ 5,5: 2x2"}}, 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Part1(tt.args.lines); got != tt.want {
+				t.Errorf("Part1() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
