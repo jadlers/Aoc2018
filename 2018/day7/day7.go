8d110cc8c22b08909327c547df5f668bd86a51e3
Jacob Adlers <jacob.adlers@gmail.com>
2018-12-07 11:48:54 +0100

    Finish 2018 D7P2
    I'll tell you one thing, it's not pretty...

--- 2018/day7/day7.go
+++ 2018/day7/day7.go
@@ -8,8 +8,10 @@ import (
 )
 
 func main() {
+	const Workers = 5
+	const ExtraTimePerStep = 60
 	input := util.ReadLines()
-	p1, p2 := Day7(input)
+	p1, p2 := Day7(input, Workers, ExtraTimePerStep)
 
 	fmt.Printf("Part 1: %v\n", p1)
 	fmt.Printf("Part 2: %v\n", p2)
@@ -28,7 +30,7 @@ type Block struct {
 
 type BlockSlice []Block
 
-func Day7(lines []string) (p1 string, p2 int) {
+func Day7(lines []string, workers, extraTimePerStep int) (p1 string, p2 int) {
 	allNames := map[string]bool{}
 	blocking := BlockSlice{}
 	for _, line := range lines {
@@ -57,7 +59,100 @@ func Day7(lines []string) (p1 string, p2 int) {
 		}
 	}
 
+	// PART 2
+	// reset blocks
+	for i, b := range blocking {
+		blocking[i] = Block{b.blocking, b.node, true}
+	}
+
+	// nextAvailableTime[worker_id]time_step_after_finish_current_job
+	workerInstruction := map[int]string{}
+	nextAvailableTime := map[int]int{}
+	for i := 0; i < workers; i++ {
+		nextAvailableTime[i] = 0
+		workerInstruction[i] = ""
+	}
+
+	instructionsDone := ""
+	// So that I can use nextStep
+	instructionsLeft := map[string]bool{}
+	for _, char := range strings.Split(p1, "") {
+		instructionsLeft[char] = true
+	}
+	for step := 0; len(nextAvailableTime) != 0; step++ {
+		// fmt.Println()
+		// fmt.Println("Step: ", step)
+		// fmt.Println("before workerInstruction: ", workerInstruction)
+		// fmt.Println("before nextAvailableTime: ", nextAvailableTime)
+
+		availableWorkers := []int{}
+		for worker, time := range nextAvailableTime {
+			// See if any worker finished this step and mark that instruction as done
+			if step == time {
+				instructionsDone += workerInstruction[worker]
+				delete(instructionsLeft, workerInstruction[worker])
+				for i, b := range blocking {
+					if workerInstruction[worker] == b.blocking {
+						blocking[i] = Block{b.blocking, b.node, false}
+					}
+				}
+				workerInstruction[worker] = ""
+
+			}
+
+			// Find workers for next step
+			if step >= time {
+				availableWorkers = append(availableWorkers, worker)
+			}
+		}
+		// fmt.Println("Blocking: ", blocking)
+		// fmt.Println("availableWorkers: ", availableWorkers)
+		// fmt.Println("Remaining: ", instructionsLeft)
+
+		// Assign work
+		for _, currWorker := range availableWorkers {
+			if len(instructionsLeft) != 0 {
+				hasNext, next := getNextStep(instructionsLeft, blocking)
+				if len(availableWorkers) > 0 && hasNext {
+					// fmt.Println("Next: ", next)
+					nextAvailableTime[currWorker] = step + extraTimePerStep + InstructionTime(next[0])
+					workerInstruction[currWorker] = next
+					delete(instructionsLeft, next)
+				}
+			} else { // When all instructions are handed out delete workers as they finish
+				delete(nextAvailableTime, currWorker)
+			}
+		}
+		// fmt.Println("Instructions done: ", instructionsDone)
+		// fmt.Println("after workerInstruction: ", workerInstruction)
+		// fmt.Println("after nextAvailableTime: ", nextAvailableTime)
+
+		p2++
+	}
+
+	// The check is done after p2 increases
+	p2--
+
 	return
+}
+
+func InstructionTime(letter uint8) int {
+	return int(letter) - 64
+}
+
+// Strikingly similair to nextStep TODO: FIX
+func getNextStep(names map[string]bool, blocking BlockSlice) (bool, string) {
+	candidates := []string{}
+	for name, _ := range names {
+		if !blocking.isBlocked(name) {
+			candidates = append(candidates, name)
+		}
+	}
+
+	if len(candidates) == 0 {
+		return false, ""
+	}
+	return true, findFirst(candidates)
 }
 
 func nextStep(names map[string]bool, blocking BlockSlice) string {
--- 2018/day7/day7_test.go
+++ 2018/day7/day7_test.go
@@ -1,10 +1,23 @@
 package main
 
-import "testing"
+import (
+	"testing"
+	"fmt"
+)
 
 func TestDay7(t *testing.T) {
+	const visualRepresentation = `
+  -->A--->B--
+ /    \      \
+C      -->D----->E
+ \           /
+  ---->F-----`
+	fmt.Println(visualRepresentation)
+
 	type args struct {
-		lines []string
+		lines            []string
+		Workers          int
+		ExtraTimePerStep int
 	}
 	tests := []struct {
 		name   string
@@ -20,16 +33,41 @@ func TestDay7(t *testing.T) {
 			"Step B must be finished before step E can begin.",
 			"Step D must be finished before step E can begin.",
 			"Step F must be finished before step E can begin.",
-		}}, "CABDFE", 0},
+		}, 2, 0}, "CABDFE", 15},
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			gotP1, gotP2 := Day7(tt.args.lines)
+			gotP1, gotP2 := Day7(tt.args.lines, tt.args.Workers, tt.args.ExtraTimePerStep)
 			if gotP1 != tt.wantP1 {
 				t.Errorf("Day7() gotP1 = '%v', want '%v'", gotP1, tt.wantP1)
 			}
 			if gotP2 != tt.wantP2 {
 				t.Errorf("Day7() gotP2 = %v, want %v", gotP2, tt.wantP2)
+			}
+		})
+	}
+}
+
+func TestInstructionTime(t *testing.T) {
+	testChars := "ABF"
+	type args struct {
+		letter uint8
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"A", args{testChars[0]}, 1},
+		{"B", args{testChars[1]}, 2},
+		{"F", args{testChars[2]}, 6},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := InstructionTime(tt.args.letter); got != tt.want {
+				t.Errorf("Instructiontime() = %v, want %v", got, tt.want)
 			}
 		})
 	}
